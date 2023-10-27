927030ade0f8a6d8406c1dfdcb84a41124a1eaa4
Teayeon-Lim <wingofyeon@naver.com>
2023-10-27 15:44:04 +0900

    Update/ Navigator 관련 변경 - add/ 미구현 "/Naver" 경로 예외 처리 - add/ "Naver_CLIENT" 변수 추출 함수

--- src/components/Navigator/index.tsx
+++ src/components/Navigator/index.tsx
@@ -1,11 +1,11 @@
 'use client';
 
-import { useState, useEffect, MouseEvent } from 'react';
+import { useState, useEffect, MouseEvent, Fragment } from 'react';
 import Link from 'next/link';
 
 import { CgPushChevronLeft, CgPushChevronRight } from 'react-icons/cg';
 
-import styles from './navigator.module.scss';
+import styles from './index.module.scss';
 import classnames from 'classnames/bind';
 const cx = classnames.bind(styles);
 
@@ -33,24 +33,41 @@ const Navigator = () => {
     return (
       <nav className={cx('navi-wrapper')} onClick={onCloseModal}>
         <ol className={cx('navi')}>
-          {NAVIGATOR_LINKS?.map(({ path, name }) => {
+          {NAVIGATOR_LINKS?.map(({ path, name, subPaths }) => {
             if (path === '/') {
               return (
                 <li
-                  key={'nav_' + path}
+                  key={'nav_/home'}
                   className={cx('home')}
                   onClick={handleIsOpenNavi}
                 >
-                  <Link href={'/'}>ALL</Link>
+                  <Link href={'/'}>All</Link>
 
                   <CgPushChevronLeft />
                 </li>
               );
             } else {
               return (
-                <li key={'nav_' + path} onClick={handleIsOpenNavi}>
-                  <Link href={path}>{name}</Link>
-                </li>
+                <Fragment key={'nav_' + path}>
+                  {/* "/Naver" 경로 완성 후, 제거 */}
+                  {path !== '/Naver' && (
+                    <li onClick={handleIsOpenNavi}>
+                      <Link href={path}>{name}</Link>
+                    </li>
+                  )}
+
+                  {!!subPaths &&
+                    subPaths.map(({ path: subPath, name: subName }) => (
+                      <li
+                        key={'subNav_' + path + subPath}
+                        onClick={handleIsOpenNavi}
+                      >
+                        <Link href={path + subPath} className={cx('sub')}>
+                          N.{subName}
+                        </Link>
+                      </li>
+                    ))}
+                </Fragment>
               );
             }
           })}
--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,9 +1,19 @@
-export const NAVIGATOR_LINKS: Array<{
+type NaviPaths = {
   path: string;
   name: string;
   createdBy: string;
-}> = JSON.parse(JSON.stringify(process.env.navigatorLinks));
+  subPaths?: NaviPaths;
+}[];
+
+export const NAVIGATOR_LINKS: NaviPaths = JSON.parse(
+  JSON.stringify(process.env.navigatorLinks)
+);
 
 export const DOMAIN_URL: string = JSON.parse(
   JSON.stringify(process.env.domainURL)
 );
+
+export const NAVER_CLIENT: {
+  clientID: string;
+  secret: string;
+} = JSON.parse(JSON.stringify(process.env.naverClient));
