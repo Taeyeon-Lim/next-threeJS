885996d4b7843b9f3f00073dc3854b59c759595a
Teayeon-Lim <wingofyeon@naver.com>
2023-10-30 19:02:34 +0900

    Update/ Navigator component, pointer hover - 현재 pathname 표시 및 클릭 방지

--- src/components/Navigator/index.tsx
+++ src/components/Navigator/index.tsx
@@ -1,6 +1,7 @@
 'use client';
 
-import { useState, useEffect, MouseEvent, Fragment } from 'react';
+import { useState, useEffect, MouseEvent, Fragment, useCallback } from 'react';
+import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 
 import { CgPushChevronLeft, CgPushChevronRight } from 'react-icons/cg';
@@ -14,10 +15,13 @@ import { NAVIGATOR_LINKS } from 'src/utils/env';
 const Navigator = () => {
   const [isOpenNavi, setIsOpenNavi] = useState(false);
 
-  const handleIsOpenNavi = () => setIsOpenNavi(prev => !prev);
+  const handleIsOpenNavi = useCallback(() => setIsOpenNavi(prev => !prev), []);
 
-  const onCloseModal = (e: MouseEvent<HTMLDivElement>) =>
-    e.target === e.currentTarget && setIsOpenNavi(false);
+  const onCloseModal = useCallback(
+    (e: MouseEvent<HTMLDivElement>) =>
+      e.target === e.currentTarget && setIsOpenNavi(false),
+    []
+  );
 
   useEffect(() => {
     const body = document.querySelector('body') as HTMLElement;
@@ -28,6 +32,8 @@ const Navigator = () => {
       body.style.removeProperty('overflow');
     }
   }, [isOpenNavi]);
+
+  const pathname = usePathname();
 
   if (isOpenNavi) {
     return (
@@ -41,32 +47,54 @@ const Navigator = () => {
                   className={cx('home')}
                   onClick={handleIsOpenNavi}
                 >
-                  <Link href={'/'}>All</Link>
+                  <Link
+                    href={'/'}
+                    className={cx({ current: pathname === '/' })}
+                    onClick={e => pathname === '/' && e.stopPropagation()}
+                  >
+                    All
+                  </Link>
 
                   <CgPushChevronLeft />
                 </li>
               );
             } else {
+              const subPath_prefix = path.slice(1, 2) + '.';
+
               return (
                 <Fragment key={'nav_' + path}>
                   {/* "/Naver" 경로 완성 후, 제거 */}
                   {path !== '/Naver' && (
                     <li onClick={handleIsOpenNavi}>
-                      <Link href={path}>{name}</Link>
+                      <Link
+                        href={path}
+                        className={cx({ current: pathname === path })}
+                        onClick={e => pathname === path && e.stopPropagation()}
+                      >
+                        {name}
+                      </Link>
                     </li>
                   )}
 
-                  {!!subPaths &&
-                    subPaths.map(({ path: subPath, name: subName }) => (
-                      <li
-                        key={'subNav_' + path + subPath}
-                        onClick={handleIsOpenNavi}
+                  {subPaths?.map(({ path: subPath, name: subName }) => (
+                    <li
+                      key={'subNav_' + path + subPath}
+                      onClick={handleIsOpenNavi}
+                    >
+                      <Link
+                        href={path + subPath}
+                        className={cx('sub', {
+                          current: pathname === path + subPath,
+                        })}
+                        onClick={e =>
+                          pathname === path + subPath && e.stopPropagation()
+                        }
                       >
-                        <Link href={path + subPath} className={cx('sub')}>
-                          N.{subName}
-                        </Link>
-                      </li>
-                    ))}
+                        <span>{subPath_prefix}</span>
+                        <span>{subName}</span>
+                      </Link>
+                    </li>
+                  ))}
                 </Fragment>
               );
             }
